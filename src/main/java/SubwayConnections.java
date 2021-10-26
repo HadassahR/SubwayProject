3bda5899cb2efca0e1fa3efdeea3a1b3c2fc8d1c
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-10-26 15:38:27 -0400

    Added getDirectConnections()

--- src/main/java/SubwayConnections.java
+++ src/main/java/SubwayConnections.java
@@ -1,3 +1,5 @@
+import javafx.util.Pair;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -5,27 +7,40 @@ import java.util.Map;
 
 public class SubwayConnections {
 
-    public Map<String, List<String>> mapConnections() throws IOException {
+    public Map<String, List<String>> mapConnections() {
         Map<String, List<String>> connections = new HashMap<>(); // Station --> list of lines
-        Converter converter = new Converter();
-        SubwayStation subwayStation = converter.convertJsonToStation();
-        SubwayLine subwayLine = converter.convertJsonToLine();
+        try {
+            Converter converter = new Converter();
+            SubwayStation subwayStation = converter.convertJsonToStation();
+            SubwayLine subwayLine = converter.convertJsonToLine();
 
-        for (int ix = 0; ix < subwayStation.features.size(); ix++){
-            for (String station : subwayStation.features.get(ix).properties.getLines())
-            connections.put(station, subwayLine.returnLinesForStation(station));
+            for (int ix = 0; ix < subwayStation.features.size(); ix++){
+                for (String station : subwayStation.features.get(ix).properties.getLines())
+                    connections.put(station, subwayLine.returnLinesForStation(station));
+            }
+        } catch (IOException exc) {
+            exc.printStackTrace();
         }
         return connections;
     }
-    // TODO Order the ObjectIds in SubwayLines
 
-    public static void main(String[] args) throws IOException {
-        SubwayConnections subwayConnections = new SubwayConnections();
-        Map <String, List<String>> sc = subwayConnections.mapConnections();
-
-        for (Map.Entry<String,List<String>> entry : sc.entrySet())
-            System.out.println("Line = " + entry.getKey() +
-                    ", Stops = " + entry.getValue());
+    public Map<String, Pair<String, String>> getDirectConnections (String station) {
+        Map<String, List<String>> connections = mapConnections();
+        Map<String, Pair<String, String>> connectionResult = new HashMap<>();
+        String previous = null;
+        String next = null;
+        for (Map.Entry<String,List<String>> entry : connections.entrySet()){
+            if (entry.getValue().contains(station)) {
+                if (!(entry.getValue().indexOf(station) == 0)){
+                    previous = entry.getValue().get(entry.getValue().indexOf(station) - 1);
+                }
+                if(!(entry.getValue().indexOf(station) == entry.getValue().size() - 1)){
+                    next = entry.getValue().get(entry.getValue().indexOf(station) + 1);
+                }
+                connectionResult.put(entry.getKey(), new Pair(previous, next));
+            }
+        }
+        return connectionResult;
     }
 }
 
