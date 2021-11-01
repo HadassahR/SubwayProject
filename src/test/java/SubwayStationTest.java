fb4ffbc48bd4c6a5c8e6f457dcff8ba6afcb578c
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-11-01 09:14:29 -0400

    Started SubwayStation test

--- src/test/java/SubwayStationTest.java
+++ src/test/java/SubwayStationTest.java
@@ -1,6 +1,8 @@
+import org.junit.Assert;
 import org.junit.Test;
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -14,19 +16,41 @@ public class SubwayStationTest {
     public void getConnectionsBeginning () throws IOException {
         // given
         Converter converter = new Converter();
+        SubwayStation.Station currentStation = converter.getStations().get(55);
+        List <Integer> expectedConnections = Arrays.asList(156);
 
         // when
+        List<Integer> receivedConnections = currentStation.getConnections(converter, currentStation);
 
         // then
+        Assert.assertEquals(expectedConnections, receivedConnections);
     }
 
     @Test
-    public void getConnectionsEnd () {
+    public void getConnectionsEnd () throws IOException {
+        // given
+        Converter converter = new Converter();
+        SubwayStation.Station currentStation = converter.getStations().get(127); //
+        List <Integer> expectedConnections = Arrays.asList(8, 447, 8, 447); //
 
+        // when
+        List<Integer> receivedConnections = currentStation.getConnections(converter, currentStation);
+
+        // then
+        Assert.assertEquals(expectedConnections, receivedConnections);
     }
 
     @Test
-    public void getConnectionsMiddle () {
+    public void getConnectionsMiddle () throws IOException {
+        // given
+        Converter converter = new Converter();
+        SubwayStation.Station currentStation = converter.getStations().get(270); //
+        List <Integer> expectedConnections = Arrays.asList(431); //
 
+        // when
+        List<Integer> receivedConnections = currentStation.getConnections(converter, currentStation);
+
+        // then
+        Assert.assertEquals(expectedConnections, receivedConnections);
     }
 }
