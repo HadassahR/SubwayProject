9fd497ae66a810af5dbbee40984a5f646c9dda90
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-11-01 21:55:57 -0400

    calculateClosestStations()

--- src/main/java/ClosestSubwayStations.java
+++ src/main/java/ClosestSubwayStations.java
@@ -1,16 +1,26 @@
-import java.io.IOException;
-import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 public class ClosestSubwayStations {
 
-    public double calculateDistanceFormula (List<Double> originCoords, List<Double> destinationCoords) {
-        double calculateLatitude = Math.pow((destinationCoords.get(0) - originCoords.get(0)), 2);
-        double calculateLongitude = Math.pow((destinationCoords.get(1) - originCoords.get(1)), 2);
+    public double calculateDistanceFormula (List<Double> locationCoords, List<Double> stationCoords) {
+        double calculateLatitude = Math.pow((stationCoords.get(0) - locationCoords.get(0)), 2);
+        double calculateLongitude = Math.pow((stationCoords.get(1) - locationCoords.get(1)), 2);
         return Math.sqrt(calculateLatitude + calculateLongitude);
     }
 
-    public void  calculateClosestStations () {
+    public List<Double> calculateClosestStations (List<Double> originCoords, List<Double> destinationCoords, SubwayStation subwayStation) {
+        double shortestFromOrigin = Double.MAX_VALUE;
+        double shortestFromDestination = Double.MAX_VALUE;
 
+        for (SubwayStation.Station station : subwayStation.getStations()) {
+            if (calculateDistanceFormula(originCoords, station.getGeometry().getCoordinates()) < shortestFromOrigin){
+                shortestFromOrigin = subwayStation.getStations().indexOf(station);
+            }
+            if (calculateDistanceFormula(destinationCoords, station.getGeometry().getCoordinates()) < shortestFromDestination) {
+                shortestFromDestination = subwayStation.getStations().indexOf(station);
+            }
+        }
+        return Arrays.asList(shortestFromOrigin, shortestFromDestination);
     }
 }
