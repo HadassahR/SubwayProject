74a75ecb1f1372e9d5a5e286ac1ec0cc5c6bded9
HadassahR <58051299+HadassahR@users.noreply.github.com>
2021-11-07 16:01:13 -0500

    More work on Dijkstra

--- src/main/java/SubwayNetwork.java
+++ src/main/java/SubwayNetwork.java
@@ -1,4 +1,7 @@
+import com.sun.org.apache.bcel.internal.generic.FSUB;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,8 +11,8 @@ public class SubwayNetwork {
 
     public void dijkstraAlgorithm (SubwayStation.Station origin, SubwayStation.Station destination) throws IOException {
         Converter converter = new Converter();
-        List<SubwayStation.Station> unvisitedNodes = origin.getConnections(converter, origin);
-        unvisitedNodes.add(0, origin);
+        List<SubwayStation.Station> unvisitedNodes = converter.getStationsList();
+        List<SubwayStation.Station> visitedNodes = new ArrayList<>();
 
         for (SubwayStation.Station station: unvisitedNodes) {
             station.getProperties().setVisited(false);
@@ -17,18 +20,32 @@ public class SubwayNetwork {
             station.getProperties().setDistance(Integer.MAX_VALUE);
         }
 
-        SubwayStation.Station currentStation = unvisitedNodes.get(0);
-        currentStation.getProperties().setCurrent(true);
+        SubwayStation.Station currentStation = unvisitedNodes.get(unvisitedNodes.indexOf(origin));
+        SubwayStation.Station destinationStation = unvisitedNodes.get(unvisitedNodes.indexOf(destination));
+
         currentStation.getProperties().setDistance(0);
 
-        for (int ix = 1; ix < unvisitedNodes.size(); ix++) {
-            int tentativeDistance = currentStation.getProperties().getDistance() + 1;
-            if (tentativeDistance < unvisitedNodes.get(ix).getProperties().getDistance()) {
-                unvisitedNodes.get(ix).getProperties().setDistance(tentativeDistance);
+        while (!destinationStation.getProperties().isVisited()){
+            currentStation.getProperties().setCurrent(true);
+            List<SubwayStation.Station> currentNodeNeighbors = currentStation.getConnections(converter, currentStation);
+            for (SubwayStation.Station neighboringStation : currentNodeNeighbors) {
+                int tentativeDistance = currentStation.getProperties().getDistance() + 1;
+                if (tentativeDistance < neighboringStation.getProperties().getDistance()) {
+                    neighboringStation.getProperties().setDistance(tentativeDistance);
+                }
+                currentStation.getProperties().setCurrent(false);
+                currentStation.getProperties().setVisited(true);
+                visitedNodes.add(currentStation);
+                unvisitedNodes.remove(currentStation); // returns bool
+            }
+            int smallestDistance = Integer.MAX_VALUE;
+            for (SubwayStation.Station station : unvisitedNodes) {
+                if (station.getProperties().getDistance() < smallestDistance){
+                    smallestDistance = station.getProperties().getDistance();
+                    currentStation = station;
+                }
             }
         }
-
-
     }
     public void tracePath () {
 
